50b5c2bafbc569e64b8f84291d0e97ff97a7a532
= <alejandro.lr.developer@hotmail.com>
2024-03-22 13:34:27 -0500

    Add: 12-bucles

---
+++ 12-bucles.py
@@ -0,0 +1,13 @@
+lenguajes = ['Python', 'Ruby', 'PHP', 'JavaScript', 'Java']
+
+# While
+i = 0
+
+while i < len(lenguajes):
+  print(lenguajes[i])
+  i = i + 1
+
+# For
+for i in lenguajes:
+  print(i)
+  
