aa40c97386cb41a5c1128de4bfa6ac473c284011
= <alejandro.lr.developer@hotmail.com>
2024-03-22 13:22:41 -0500

    Add: 11-listas-metodos

---
+++ 11-listas-metodos.py
@@ -0,0 +1,7 @@
+lenguajes = ['Python', 'Ruby', 'PHP', 'JavaScript', 'Java']
+
+lenguajes.insert(3, 'Go')
+lenguajes.insert(0, 'C')
+lenguajes.remove('PHP')
+print('JavaScript' in lenguajes)
+print(lenguajes)
