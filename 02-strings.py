4606a6791c31caeb489ca37776e5caf60dec96eb
= <alejandro.lr.developer@hotmail.com>
2024-03-22 12:00:39 -0500

    Add: 02-strings methods

--- 02-strings.py
+++ 02-strings.py
@@ -1,3 +1,11 @@
 texto = "Hola Mundo"
 
-print(texto)
+print(texto.upper())
+print(texto.lower())
+print(texto.find('M'))
+
+nuevo_texto = texto.replace('Mun', 'Run')
+
+print(texto, nuevo_texto)
+
+print('Mundo' in texto)
